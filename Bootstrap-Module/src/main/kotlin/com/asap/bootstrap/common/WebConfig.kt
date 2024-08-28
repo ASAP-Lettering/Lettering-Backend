d1176acea026d4613e2756364cdd33f570585c57
Sim-km <simkm0824@naver.com>
2024-08-29 00:12:06 +0900

    ASAP-60 feat: cors 설정 추가

---
+++ Bootstrap-Module/src/main/kotlin/com/asap/bootstrap/common/WebConfig.kt
@@ -0,0 +1,17 @@
+package com.asap.bootstrap.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+    }
+
+}
