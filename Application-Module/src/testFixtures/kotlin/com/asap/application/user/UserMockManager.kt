bc28e732f21336eb703063a5d1d7137f1545bf90
Sim-km <simkm0824@naver.com>
2024-09-02 21:27:43 +0900

    ASAP-97 test: 테스트 패키지 경로 java->kotlin으로 수정

--- Application-Module/src/testFixtures/kotlin/com/asap/application/user/UserMockManager.kt
+++ Application-Module/src/testFixtures/kotlin/com/asap/application/user/UserMockManager.kt
@@ -2,7 +2,6 @@ package com.asap.application.user
 
 import com.asap.application.user.port.out.UserTokenManagementPort
 import com.asap.domain.user.entity.UserToken
-import com.asap.domain.user.enums.TokenType
 
 
 class UserMockManager(
@@ -16,7 +15,6 @@ class UserMockManager(
         tokenManagementPort.saveUserToken(
             UserToken(
                 token = token,
-                type = TokenType.ACCESS
             )
         )
     }
