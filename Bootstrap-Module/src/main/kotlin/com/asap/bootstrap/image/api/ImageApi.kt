af651350e0fe48f2e9edeeade56153fd15ee3652
Sim-km <simkm0824@naver.com>
2024-09-10 22:35:59 +0900

    ASAP-115 fix: 이미지 업로드 content-type 명시

--- Bootstrap-Module/src/main/kotlin/com/asap/bootstrap/image/api/ImageApi.kt
+++ Bootstrap-Module/src/main/kotlin/com/asap/bootstrap/image/api/ImageApi.kt
@@ -2,18 +2,25 @@ package com.asap.bootstrap.image.api
 
 import com.asap.bootstrap.common.security.annotation.AccessUser
 import com.asap.bootstrap.image.dto.UploadImageResponse
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.headers.Header
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
 
+@Tag(name = "Image", description = "Image API")
 @RequestMapping("/api/v1/images")
 interface ImageApi {
 
-    @PostMapping()
+    @Operation(
+        summary = "이미지 업로드",
+        description = "이미지를 업로드합니다.",
+    )
+    @PostMapping(consumes = ["multipart/form-data"])
     @ApiResponses(
         value = [
             ApiResponse(
