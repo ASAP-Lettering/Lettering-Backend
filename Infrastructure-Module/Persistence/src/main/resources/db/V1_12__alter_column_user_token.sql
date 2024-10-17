0c7f82a3a002203dde02d9de1d81fa62f758b598
Sim-km <simkm0824@naver.com>
2024-10-17 23:06:52 +0900

    ASAP-000 fix: 토큰 타입 변경

--- Infrastructure-Module/Persistence/src/main/resources/db/V1_12__alter_column_user_token.sql
+++ Infrastructure-Module/Persistence/src/main/resources/db/V1_12__alter_column_user_token.sql
@@ -1,1 +1,7 @@
+alter table user_token drop constraint uk_token_index;
+alter table user_token drop constraint uc_user_token_token;
+
+
 alter table user_token modify column token text not null;
+
+create unique index uk_token_index on user_token (token(756));
