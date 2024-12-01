a2264b7b7561de2209627f3601793866bc5cdf72
Sim-km <simkm0824@naver.com>
2024-12-01 17:39:14 +0900

    ASAP-304 fix: 테스트 수정

--- Application-Module/src/testFixtures/kotlin/com/asap/application/user/UserApplicationConfig.kt
+++ Application-Module/src/testFixtures/kotlin/com/asap/application/user/UserApplicationConfig.kt
@@ -2,16 +2,14 @@ package com.asap.application.user
 
 import com.asap.application.user.port.out.UserAuthManagementPort
 import com.asap.application.user.port.out.UserManagementPort
-import com.asap.application.user.port.out.UserTokenManagementPort
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 
 @TestConfiguration
 class UserApplicationConfig(
-    private val userTokenManagementPort: UserTokenManagementPort,
     private val userManagementPort: UserManagementPort,
     private val userAuthManagementPort: UserAuthManagementPort,
 ) {
     @Bean
-    fun userMockGenerator(): UserMockManager = UserMockManager(userTokenManagementPort, userManagementPort, userAuthManagementPort)
+    fun userMockGenerator(): UserMockManager = UserMockManager(userManagementPort, userAuthManagementPort)
 }
